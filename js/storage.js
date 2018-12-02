938abd510591905b3d2291117ef518ebcd38c81c
kohdakaminotsuki <44516591+kohdakaminotsuki@users.noreply.github.com>
2018-12-01 20:40:33 -0500

    Update storage.js
    moved function backupdownload to storage.js so that this file can be moved between javascript frameworks better for the intended "passport" system. Also switched let back to avoid incompatibility issues with some frameworks and the fact that it is easier to pull of weird things as a variable compared with the more restricted let from what I've seen

--- js/storage.js
+++ js/storage.js
@@ -1,8 +1,12 @@
-"use strict";
-// Persistent Storage Variable
-
-let storage = {
+var storage = {
 	player: {
 		name: ""
 	}
+};
+
+function backupdownload(){
+	var backupdownload = document.createElement('a');
+	backupdownload.href = 'data:attachment/text,' + "storage = " + "JSON.parse('" +  encodeURI( (JSON.stringify(storage)) ) + "')"+"/"+"/"+ encodeURI("If the game asks for your player passport as an input copy and paste everything after storage=, otherwise copy and paste everything here into the console";
+	backupdownload.download = 'backupstorage.txt';
+	backupdownload.click();
 };
