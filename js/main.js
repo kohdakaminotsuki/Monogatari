cc3ad6d468b97e785c494493d1ece3fb100fd769
kohdakaminotsuki <44516591+kohdakaminotsuki@users.noreply.github.com>
2018-12-01 20:41:26 -0500

    Update main.js
    removed backupdownload to place it in the storage.js file

--- js/main.js
+++ js/main.js
@@ -3,12 +3,3 @@
  * Your Javascript Code Goes Here
  * ==============================
  **/
-
-//function backupdownload(); when entered into the console provides the command to overwrite the storage variable of another
-// instance of the game with the data from when the backupdownload function was written
-function backupdownload(){
-	var backupdownload = document.createElement('a');
-	backupdownload.href = 'data:attachment/text,' + "storage = " + "JSON.parse('" +  encodeURI( (JSON.stringify(storage)) ) + "')";
-	backupdownload.download = 'backupstorage.txt';
-	backupdownload.click();
-};
